89093b5514f2962cf1d00a5090bab911450d25ea
Ernest Soo <ernestsoo22@hotmail.com>
2019-06-27 21:47:07 +0800

    test yet again.

--- scripts/attiya_query.js
+++ scripts/attiya_query.js
@@ -479,9 +479,9 @@ jQuery(function () {
     mobile_presentation_left = mobile_presentation_left / 2;
 
     $(".home-presentation").css("margin-left",mobile_presentation_left.toString() + "px");
-    $(".home-presentation").css("margin-top", "140px");
+    $(".home-presentation").css("margin-top", "160px");
 
-    var browse_services_top = 160 + ($(window).height() * 0.4);
+    var browse_services_top = 180 + ($(window).height() * 0.4);
     
     $(".m-browse-services").css("top", browse_services_top.toString() + "px");
   }
